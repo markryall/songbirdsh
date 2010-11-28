b4aba04b1eb08c2cb3829573aa29a6806c6bf29a
Mark Ryall <mark@ryall.name>
2010-11-28 19:58:54 +1000

    added a rudimentary song queing system

--- play_random.rb
+++ play_random.rb
@@ -1,11 +1,23 @@
 require 'songbird'
 require 'splat'
 require 'cgi'
+require 'yaml'
+require 'fileutils'
+
+def next_id total_tracks
+  file = Dir.glob('*.song').sort.first
+  id = (rand * total_tracks).to_i
+  if file
+    id = YAML.load(File.read(file))[:media_item_id]
+    FileUtils.rm file
+  end
+  id 
+end
 
 Songbird.with_db do |db|
   total_tracks = db[:media_items].count
   loop do
-    id = (rand * total_tracks).to_i
+    id= next_id(total_tracks)
     row = db[:media_items][:media_item_id=>id]
     if row
       path = CGI.unescape(row[:content_url].slice(7..-1)) if row[:content_url] =~ /^file/
--- show_properties.rb
+++ show_properties.rb
@@ -1,8 +1,12 @@
 require 'songbird'
+require 'yaml'
+
+id = ARGV.shift.to_i
 
 Songbird.with_db do |db|
-  db[:resource_properties].filter(:media_item_id=>ARGV.shift.to_i).each do |row|
+  db[:resource_properties].filter(:media_item_id=>id).each do |row|
     puts row.inspect
     puts
   end
+  File.open("#{Time.now.to_i}-#{(rand*1000).to_i}.song", 'w') {|f| f.print db[:media_items][:media_item_id=>id].to_yaml }
 end
