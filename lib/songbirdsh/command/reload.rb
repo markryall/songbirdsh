b27b80eeccbaccc5d18052d31c9d1ef6932f28d7
Mark Ryall <mark@ryall.name>
2011-02-05 19:14:37 +1000

    added a spec, usage and help for reload command

--- lib/songbirdsh/command/reload.rb
+++ lib/songbirdsh/command/reload.rb
@@ -4,4 +4,12 @@ class Songbirdsh::Command::Reload < Songbirdsh::Command
   def execute ignored=nil
     @player.library.reload
   end
+
+  def usage
+    ''
+  end
+
+  def help
+    'reloads the contents of the music library for fast searching'
+  end
 end
---
+++ spec/songbirdsh/command/reload_spec.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__)+'/../../spec_helper'
+require 'songbirdsh/command/reload'
+
+describe Songbirdsh::Command::Reload do
+  extend ShellShock::CommandSpec
+
+  with_usage ''
+  with_help 'reloads the contents of the music library for fast searching'
+
+  before do
+    @library = stub('library')
+    @player = stub('player', :library => @library)
+    @command = Songbirdsh::Command::Reload.new @player
+  end
+
+  it 'should reload the library' do
+    @library.should_receive(:reload)
+    @command.execute '123'
+  end
+end
