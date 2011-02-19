069cb21cd0d17f87a0128dfb950bf2381b0bec5d
Mark Ryall <mark@ryall.name>
2011-02-19 21:30:14 +1000

    changed filenames in persisted tracks

--- lib/songbirdsh/queue.rb
+++ lib/songbirdsh/queue.rb
@@ -1,8 +1,10 @@
 module Songbirdsh
   module Queue
     def enqueue id
+      @sequence ||= 0
       @library.with_track id do |track|
-        File.open("#{Time.now.to_i}-#{id.to_s.rjust(8,'0')}.song", 'w') {|f| f.print track.to_yaml }
+        File.open("#{Time.now.to_i}-#{@sequence.to_s.rjust(8,'0')}.song", 'w') {|f| f.print track.to_yaml }
+        @sequence += 1
       end
     end
 
