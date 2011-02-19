ea3458715ec8b80791fb90f9b70d214419466d5c
Mark Ryall <mark@ryall.name>
2011-02-19 22:15:47 +1000

    added a grep search to list (to see when specific tracks are due to be played)

--- lib/songbirdsh/command/list.rb
+++ lib/songbirdsh/command/list.rb
@@ -2,12 +2,23 @@ require 'songbirdsh/command'
 
 class Songbirdsh::Command::List < Songbirdsh::Command
   def execute text
+    @terms = text.split(/\W/)
+    current = @player.current
+    next_start_time = Time.at current.started
+    show next_start_time, current
+    next_start_time += current.duration
     @player.each do |track|
-      puts track
+      show next_start_time, track
+      next_start_time += track.duration
     end
   end
 
+  def show time, track
+    return unless @terms.empty? or @terms.all? {|term| track.search_string.include? term }
+    puts "#{time}\n\t#{track}"
+  end
+
   def help
-    'lists the contents of the  queue'
+    'lists the contents of the track queue (and approximate times for when each track will be played)'
   end
 end
