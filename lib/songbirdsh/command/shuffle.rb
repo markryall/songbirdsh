1090f6a9877474053e8716aeb69ea930db876bf4
Mark Ryall <mark@ryall.name>
2011-03-05 15:52:50 +1000

    added spec for shuffle and changed it to accept ranges (as well as shuffling search results)

--- lib/songbirdsh/command/shuffle.rb
+++ lib/songbirdsh/command/shuffle.rb
@@ -1,15 +1,20 @@
 require 'songbirdsh/command'
+require 'songbirdsh/range_expander'
 
 class Songbirdsh::Command::Shuffle < Songbirdsh::Command
-  def execute text
-    if @player.matches
-      @player.matches.sort_by { rand }.join(' ').to_clipboard
-    else
-      puts 'nothing to shuffle - please search for some tracks'
-    end
+  def execute text=''
+    @expander ||= Songbirdsh::RangeExpander.new
+    values = @expander.expand text
+    values = @player.matches if values.nil? or values.empty?
+    return values.sort_by { rand }.join(' ').to_clipboard unless values.nil? or values.empty?
+    puts 'nothing to shuffle - please search for some tracks'
+  end
+
+  def usage
+    '[*<id>]'
   end
 
   def help
-    'shuffles the results from the last search and places them on the clipboard'
+    'shuffles either the results from the last search or a list of tracks and places them on the clipboard'
   end
 end
---
+++ spec/songbirdsh/command/shuffle_spec.rb
@@ -0,0 +1,51 @@
+require File.dirname(__FILE__)+'/../../spec_helper'
+require 'songbirdsh/command/shuffle'
+
+describe Songbirdsh::Command::Shuffle do
+  extend ShellShock::CommandSpec
+
+  with_usage '[*<id>]'
+  with_help 'shuffles either the results from the last search or a list of tracks and places them on the clipboard'
+
+  before do
+    @expander = stub('expander')
+    Songbirdsh::RangeExpander.stub!(:new).and_return(@expander)
+    @player = stub('player')
+    @command = Songbirdsh::Command::Shuffle.new @player
+  end
+
+  it 'should report warning if there are no arguments and there are no matches' do
+    @player.should_receive(:matches).and_return nil
+    @expander.should_receive(:expand).with('').and_return nil
+    @command.should_receive(:puts).with 'nothing to shuffle - please search for some tracks'
+
+    @command.execute
+  end
+
+  def should_randomise_values values
+    sorted_values = stub 'values'
+    joined_values = stub 'joined values'
+    values.should_receive(:sort_by).and_return sorted_values
+    sorted_values.should_receive(:join).with(' ').and_return joined_values
+    joined_values.should_receive :to_clipboard
+  end
+
+  it 'should shuffle the player matches (results from the last search)' do
+    matches = [1,2,3]
+
+    @expander.should_receive(:expand).with('').and_return []
+    @player.should_receive(:matches).and_return matches
+    should_randomise_values matches
+
+    @command.execute
+  end
+
+  it 'should expand and shuffle any arguments' do
+    values = [1,2,3]
+
+    @expander.should_receive(:expand).with('some text').and_return values
+    should_randomise_values values
+
+    @command.execute 'some text'
+  end
+end
