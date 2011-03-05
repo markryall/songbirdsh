0d51b95edc6bf2fd06f94bdc72bb3c2ad9b7d9b1
Mark Ryall <mark@ryall.name>
2011-03-05 13:40:15 +1000

    replaced enqueue examples with a mock test against the expander

--- spec/songbirdsh/command/enqueue_spec.rb
+++ spec/songbirdsh/command/enqueue_spec.rb
@@ -12,33 +12,13 @@ describe Songbirdsh::Command::Enqueue do
     @command = Songbirdsh::Command::Enqueue.new @player
   end
 
-  def should_queue *ids
-    ids.each {|id| @player.should_receive(:enqueue).with id.to_i(36) }
-  end
+  it 'should enqueue whatever is returned from the expander' do
+    expander = stub('expander')
+    Songbirdsh::RangeExpander.should_receive(:new).and_return(expander)
+    expander.should_receive(:expand).with('some text').and_return([1,2,3])
 
-  it 'should enqueue a single track' do
-    should_queue '123'
-    @command.execute '123'
-  end
+    [1,2,3].each {|id| @player.should_receive(:enqueue).with id}
 
-  it 'should enqueue multiple tracks separated by any non digit' do
-    should_queue *%w{123 456 789}
-    @command.execute "123 \t 456  , 789 "
-  end
-
-  it 'should enqueue a fully specified range of tracks' do
-    values = (5370..5382).to_a
-    values.each do |id|
-      @player.should_receive(:enqueue).with id
-    end
-    @command.execute " 456-45i "
-  end
-
-  it 'should enqueue an abbreviated range of tracks' do
-    values = (5370..5382).to_a
-    values.each do |id|
-      @player.should_receive(:enqueue).with id
-    end
-    @command.execute " 456-i "
+    @command.execute "some text"
   end
 end
