ac19b0ac2dca62e166106bbc8636c74da4f6c996
Mark Ryall <mark@ryall.name>
2011-06-19 17:51:19 +1000

    puts considered harmful

--- lib/songbirdsh/command/flush.rb
+++ lib/songbirdsh/command/flush.rb
@@ -3,5 +3,5 @@ require 'songbirdsh/command'
 class Songbirdsh::Command::Flush
   include Songbirdsh::Command
   help 'flushes the current queue'
-  execute {|ignored| loop { break unless @player.dequeue; puts 'not yet' } }
+  execute {|ignored| loop { break unless @player.dequeue } }
 end
